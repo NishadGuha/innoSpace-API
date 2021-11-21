0813298128c2a081172086b5d90497d8ceaca0e6
Nishad Guha <nishadguha@Nishads-MacBook-Pro.local>
2021-11-21 20:57:31 +0100

    feat(update-patch)

--- src/Controller/NeighborhoodController.php
+++ src/Controller/NeighborhoodController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Neighborhood;
 use App\Form\NeighborhoodType;
+use App\Form\NeighborhoodUpdateType;
 use App\Repository\NeighborhoodRepository;
 use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Utils\SerializerUtil;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class NeighborhoodController extends AbstractApiController
 {
@@ -38,6 +40,33 @@ class NeighborhoodController extends AbstractApiController
         $neighborhoodSerialized = $serializer->serialize($neighborhoods, 'json');
 
         return new Response($neighborhoodSerialized, 200, []);
+    }
+
+    public function findByIdAction(Request $request, int $id): Response
+    {
+        $neighborhood = $this->neighborhoodRepository->findById($id);
+
+        $serializer = SerializerUtil::serializer();
+
+        $neighborhoodSerialized = $serializer->serialize($neighborhood, 'json');
+
+        return new Response($neighborhoodSerialized, 200, []);
+    }
+
+    public function removeByIdAction(Request $request, int $id): Response
+    {
+        $neighborhood = $this->neighborhoodRepository->findById($id);
+        $neighborhoodId = $neighborhood->getId();
+
+        $this->neighborhoodRepository->remove($neighborhood);
+
+        $response = [
+            "status" => 200,
+            "message" => "success",
+            "description" => "Neighborhood ". $neighborhoodId . " at " . $neighborhood->getStreet() . " removed."
+        ];
+
+        return new Response(json_encode($response), 200, []);
     }
 
     /**
@@ -92,5 +121,36 @@ class NeighborhoodController extends AbstractApiController
             "description" => "Deleted all neighborhoods"
         ]);
 
+    }
+
+    public function updateAction(Request $request, int $id)
+    {
+        $neighborhood = $this->neighborhoodRepository->findById($id);
+
+        if (!$neighborhood) {
+            throw new NotFoundHttpException('Neighborhood not found');
+        }
+
+        $form = $this->buildForm(NeighborhoodType::class, $neighborhood, [
+            'method' => $request->getMethod()
+        ]);
+
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            print 'ERROR: Data Invalid';
+            exit;
+        }
+
+        /** @var Neighborhood $data */
+        $data = $form->getData();
+
+        $this->neighborhoodRepository->save($data);
+
+        $serializer = SerializerUtil::serializer();
+
+        $neighborhoodSerialized = $serializer->serialize($neighborhood, 'json');
+
+        return new Response($neighborhoodSerialized, 200, []);
     }
 }
